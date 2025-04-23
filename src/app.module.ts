b2c9d7a3787e690b54ec0eca594f5d33e24d56cb
lambert.li <l156486648@163.com>
2025-04-23 17:09:32 +0800

    feat: 添加静态资源访问模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { SqlModule } from './sql/sql.module';
 
 @Module({
-  imports: [AuthModule, SqlModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..', 'public'), // 图片存放目录
+      serveRoot: '/static', // 可选，设置访问前缀
+    }),
+    AuthModule, SqlModule],
   controllers: [AppController],
   providers: [AppService],
 })
